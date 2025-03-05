aa56457410a7efeb63af0b6858db60da20c14e4a
yoga <bani.leonardo.25@gmail.com>
2025-03-05 18:33:21 +0100

    0.2 Category done

---
+++ cmake-build-debug/CMakeFiles/lab_programmazione.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_programmazione.dir/main.cpp.o"
+  "CMakeFiles/lab_programmazione.dir/main.cpp.o.d"
+  "lab_programmazione"
+  "lab_programmazione.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_programmazione.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/lab_programmazione.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lab_programmazione.
+# This may be replaced when dependencies are built.
