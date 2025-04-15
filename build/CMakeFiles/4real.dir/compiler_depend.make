e69f83ec1fb9f8dd141e097920b82f4a11196c4f
Leonardo Bani <bani.leonardo.25@gmail.com>
2025-04-15 12:28:12 +0200

    Delete build directory

--- build/CMakeFiles/4real.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for 4real.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yoga/CLionProjects/lab_programmazione
-# Build directory: /home/yoga/CLionProjects/lab_programmazione/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyTests "MyTests")
-set_tests_properties(MyTests PROPERTIES  _BACKTRACE_TRIPLES "/home/yoga/CLionProjects/lab_programmazione/CMakeLists.txt;40;add_test;/home/yoga/CLionProjects/lab_programmazione/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
